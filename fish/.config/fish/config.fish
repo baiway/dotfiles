00fe08aee53c9d66c5488d1da975dbacf21e2f6c
Bailey Cook <contact@baileycook.co.uk>
2026-07-14 19:51:57 +0100

    Add separate bootstrap scripts for macOS and CachyOS

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+# Keep CachyOS's shipped defaults
+if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
+    source /usr/share/cachyos-fish-config/cachyos-config.fish
+end
+
+# --- my additions ------------------------------------------------------------
+# TODO add analogous lines to initiate `starship`, `zoxide`, set `fzf` keybindings, etc.
+# These are in `.zshrc` but need to be adapted for `config.fish`
